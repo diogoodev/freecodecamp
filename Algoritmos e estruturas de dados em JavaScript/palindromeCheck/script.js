2f4504f99658e6a645632b1ee89168cf4316a382
Diogo Moura <diogoodev@gmail.com>
2024-03-07 13:59:51 +0000

    add finish the palindrome project

--- Algoritmos e estruturas de dados em JavaScript/palindromeCheck/script.js
+++ Algoritmos e estruturas de dados em JavaScript/palindromeCheck/script.js
@@ -1,4 +1,3 @@
-// When you click on the #check-btn element without entering a value into the #text-input element, an alert should appear with the text Please input a value
 const inputCheck = document.getElementById("text-input");
 const checkButton = document.getElementById("check-btn");
 const result = document.getElementById("result");
@@ -12,30 +11,25 @@ function emptyInput() {
 }
 
 
-
-// OK
 function cleanInput() {
   const regex = /[\W_]/g;
   const cleanString = inputCheck.value.replace(regex, "");
   return cleanString;
 }
-// OK
+
 function lowercaseString(upperString) {
   const lowerString = upperString.toLowerCase();
   return lowerString;
 }
 
-// OK!
+
 function inverteInput() {
   checkButton.addEventListener("click", () => {
-    //* Limpa os caracteres indesejados da string e coloca em minusculo logo depois salva na variavel
     const lowerInput = lowercaseString(cleanInput());
-    //* transformamos a string em um array de caracteres
     let inputArray = lowerInput.slice("");
-    //* invertemos o array
     const inputArrayReverso = [...inputArray].reverse().join("");
-       
-    inputArrayReverso === lowerInput ? result.textContent = `${inputCheck.value} is a palindrome`: result.textContent = `${inputCheck.value} is NOT a palindrome` ;
+    
+    inputArrayReverso === lowerInput ? result.innerHTML = `<strong style="color: #996dff">${inputCheck.value}</strong> is a palindrome`: result.innerHTML = `<strong>${inputCheck.value}</strong> is not a palindrome` ;
     
   });
 }
